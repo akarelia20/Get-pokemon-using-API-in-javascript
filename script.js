6a5ec139241eb8cc57aa64c1183d5e717a8c742e
Amee Karelia <Amee.karelia16@gmail.com>
2022-03-20 13:24:51 -0700

    pokemon API

---
+++ script.js
@@ -0,0 +1,20 @@
+async function getpokemon(e){
+    if(pokemon_index.value %1 == 0) {
+        rotated_value = pokemon_index.value % 1125;
+        console.log(pokemon_index);
+        var response= await fetch(`https://pokeapi.co/api/v2/pokemon/${rotated_value}`);
+        var theData= await response.json()
+        console.log(theData);
+        pokemon_card.innerHTML= `
+        <h2 class="pokemon_title"> ${theData.name}  <h2>
+        <img class= "pokemon_img" src="${theData.sprites.front_default}"> </img>
+        `
+    } 
+    else {
+        pokemon_card.innerHTML=`
+        <p class="error"> 
+        Ooops!!! Not valid entry, try entering whole number..  
+        </p>
+        `
+    }
+}
